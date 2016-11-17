390c1069e562aee656ddaf3f75c66b389b3f966f
stephaneDiot <stephane.diot@ez.no>
2016-11-17 10:46:31 +0100

    EZP-25552 Explorer View

---
+++ Resources/public/js/views/universaldiscovery/ez-universaldiscoveryfinderexplorerlevelview.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) eZ Systems AS. All rights reserved.
+ * For full copyright and license information view LICENSE file distributed with this source code.
+ */
+YUI.add('ez-universaldiscoveryfinderexplorerlevelview', function (Y) {
+    "use strict";
+    /**
+     * Provides the universal discovery finder explorer level view
+     *
+     * @module ez-universaldiscoveryfinderexplorerlevelview
+     */
+    Y.namespace('eZ');
+
+    /**
+     * The universal discovery finder explorer level. It shows content of a given depth
+     *
+     * @namespace eZ
+     * @class UniversalDiscoveryFinderExplorerLevelView
+     * @constructor
+     * @extends eZ.TemplateBasedView
+     */
+    Y.eZ.UniversalDiscoveryFinderExplorerLevelView = Y.Base.create('universalDiscoveryFinderExplorerLevelView', Y.eZ.TemplateBasedView, [], {
+        initializer: function () {
+console.log('finderlevel');
+
+        },
+
+
+
+
+
+    }, {
+        ATTRS: {
+          
+        },
+    });
+});
